2f74d456ca322495f2c807e62734b6696f6bf113
PepperKick <abhishrant.m@gmail.com>
2019-07-08 00:56:07 +0530

    Added crypto

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const otplib = require("otplib")
+const otplib = require("otplib/authenticator");
+const crypto = require("crypto");
 
 module.exports = (schema, options) => {
+    otplib.options = { crypto }
+
     schema.add({
         _2faSecret: {
             type: String
@@ -8,12 +11,12 @@ module.exports = (schema, options) => {
     })
 
     schema.methods.generate2FASecret = async function () {
-        this._2faSecret = await otplib.authenticator.generateSecret();
+        this._2faSecret = await otplib.generateSecret();
 
         await this.save();
     }
     
     schema.methods.verify2FAToken = async function (token) {
-        return otplib.authenticator.check({ token, secret: this._2faSecret });
+        return otplib.check({ token, secret: this._2faSecret });
     }
 }
